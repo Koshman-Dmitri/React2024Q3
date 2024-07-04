da7cc0b417d3204a1fe9e0c0a038eeb62cc6bca6
Koshman-Dmitri <123456desher@rambler.ru>
2024-07-04 13:41:22 +0300

    fix: vite config console warning

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import checker from 'vite-plugin-checker';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  define: {
+    __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'true',
+  },
   plugins: [
     react(),
     checker({
