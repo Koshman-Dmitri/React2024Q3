a3acc2867d565255a3ed0a541488ffc82661de88
Koshman-Dmitri <123456desher@rambler.ru>
2024-07-07 17:50:13 +0300

    fix: error to console for error boundary

--- src/components/ErrorBoundary/ErrorBoundary.tsx
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -15,7 +15,7 @@ export class ErrorBoundary extends Component<Props, State> {
   }
 
   componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
-    console.log(error.toString(), errorInfo.componentStack);
+    console.error(error.toString(), errorInfo.componentStack);
   }
 
   private handleClose = (): void => this.setState({ hasError: false });
