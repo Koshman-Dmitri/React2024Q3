ad7d35e3e8e9d524c6849fb9bf339f102fab19b9
Koshman-Dmitri <123456desher@rambler.ru>
2024-07-12 15:13:16 +0300

    feat: tests for search component

--- src/tests/SearchForm.test.tsx
+++ src/tests/SearchForm.test.tsx
@@ -1,16 +1,36 @@
-import { describe, expect, test } from 'vitest';
-import { render, screen } from '@testing-library/react';
-import { ReactNode } from 'react';
-import { MemoryRouter } from 'react-router-dom';
+import { screen } from '@testing-library/react';
+import { userEvent } from '@testing-library/user-event';
 import { SearchForm } from '../components';
+import { renderWithRouter } from './utils/utils';
 import '@testing-library/jest-dom';
-
-const renderWithRouter = (component: ReactNode) => render(<MemoryRouter>{component}</MemoryRouter>);
 
 describe('SearchForm', () => {
   test('Should be rendered', () => {
     renderWithRouter(<SearchForm />);
-    const text = screen.getByText('Search astronomical object:');
+
+    const text = screen.getByText<HTMLInputElement>('Search astronomical object:');
     expect(text).toBeInTheDocument();
+  });
+
+  test('Click on Search saves value to LS', async () => {
+    renderWithRouter(<SearchForm />);
+
+    const searchBtn = screen.getByRole<HTMLButtonElement>('button');
+    const input = screen.getByLabelText<HTMLInputElement>('Search astronomical object:');
+
+    await userEvent.type(input, '12345');
+    await userEvent.click(searchBtn);
+
+    expect(input.value).toBe(JSON.parse(localStorage.getItem('prevSearch_KD')!));
+  });
+
+  test('Get value from LS on mounting', () => {
+    const testValue = 'TestValue';
+    localStorage.setItem('prevSearch_KD', testValue);
+
+    renderWithRouter(<SearchForm />);
+    const input = screen.getByLabelText<HTMLInputElement>('Search astronomical object:');
+
+    setTimeout(() => expect(input.value).toBe(testValue), 100);
   });
 });
---
+++ src/tests/utils/utils.tsx
@@ -0,0 +1,6 @@
+import { render } from '@testing-library/react';
+import { ReactNode } from 'react';
+import { MemoryRouter } from 'react-router-dom';
+
+export const renderWithRouter = (component: ReactNode) =>
+  render(<MemoryRouter>{component}</MemoryRouter>);
