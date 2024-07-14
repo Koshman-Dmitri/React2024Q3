e10b47032bbcd85de83b72d5fdb514f6c8a8fbf8
Koshman-Dmitri <123456desher@rambler.ru>
2024-07-14 10:02:54 +0300

    feat: tests for error page

--- src/components/ErrorPage/ErrorPage.tsx
+++ src/components/ErrorPage/ErrorPage.tsx
@@ -3,7 +3,7 @@ import styles from './ErrorPage.module.css';
 export function ErrorPage() {
   return (
     <div className={styles.errorPage}>
-      <h1 className={styles.title}>Not Foudnd</h1>
+      <h1 className={styles.title}>Not Found</h1>
       <p>Sorry, try correct URL</p>
     </div>
   );
