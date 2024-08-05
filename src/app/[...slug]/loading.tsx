2979f2fc957db604ac95bdaaaec35e552a0c73d5
Koshman-Dmitri <123456desher@rambler.ru>
2024-08-05 19:43:31 +0300

    refactor: loading style

--- src/app/[...slug]/loading.tsx
+++ src/app/[...slug]/loading.tsx
@@ -3,7 +3,7 @@ import { Loader } from '../../components';
 export default function Loading() {
   return (
     <>
-      <h2 style={{ fontSize: '4rem', textAlign: 'center', color: 'white' }}>Loading...</h2>
+      <h2 style={{ fontSize: '4rem', textAlign: 'center', color: 'gold' }}>Loading...</h2>
       <Loader />
     </>
   );
